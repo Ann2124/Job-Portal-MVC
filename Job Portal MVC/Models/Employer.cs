421283269b4782d507e2bac9d49af19b311a0cb0
reenyjoseph12@gmail.com <reenyjoseph12@gmail.com>
2020-11-02 17:35:42 +0530

    application,edit employer

--- Job Portal MVC/Models/Employer.cs
+++ Job Portal MVC/Models/Employer.cs
@@ -25,7 +25,15 @@ namespace Job_Portal_MVC.Models
         [StringLength(255, ErrorMessage = "Password should be minimum of 8 characters", MinimumLength = 8)]
         [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and should contain : upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string password { get; set; }
-        [Required]
+        
+        [NotMapped]
+        [System.ComponentModel.DataAnnotations.Compare("password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirm Password required")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "mobile number is required")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Your mobile number  is not valid")]
+        [RegularExpression(@"^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$", ErrorMessage = "Please enter a valid mobile number")]
         public string mobileNo { get; set; }
         [Required]
         public string Organisation { get; set; }
---
+++ Job Portal MVC/Models/application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Job_Portal_MVC.Models
+{
+    public class application
+    {
+       
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int application_id { get; set; }
+        [Required(ErrorMessage ="Email  Id required")]
+        [Display(Name = "Email Id")]
+        public string employerId { get; set; }
+        [Required(ErrorMessage = " Job Id required")]
+        [Display(Name = "Job Id")]
+        public int job_id{ get; set; }
+        public string status { get; set; }
+
+    }
+}
