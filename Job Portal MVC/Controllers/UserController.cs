8123eed7893a1c8a3bdc53065c74cf85c7e89fa8
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-11-03 09:31:08 +0530

    User Controller Created and  index method added

---
+++ Job Portal MVC/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Job_Portal_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Job_Portal_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        JobPortalContext db = new JobPortalContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User usr)
+        {
+            usr.password = encrypt(usr.password);
+            var user = db.Users.Where(a => a.email.Equals(usr.email) && a.password.Equals(usr.password)).FirstOrDefault();
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(usr.email, false);
+                Session["UserId"] = user.email.ToString();
+                Session["Username"] = (user.firstname).ToString();
+                return RedirectToAction("Index", "Job");
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid Login Credentials");
+            }
+            return View(usr);
+        }
+        public string encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (System.IO.MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+
+    }
+}
